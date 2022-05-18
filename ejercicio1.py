0889299a8c05bd6ebe7c570e0893fb364762bfcd
fjvivanco7 <105758129+fjvivanco7@users.noreply.github.com>
2022-05-18 09:59:41 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,5 @@
+#EJERCICIO 1: ingresar el radio de un circulo del usuario y calcule el area
+radio=int(input("Ingrese el radio del circulo: "))
+area=3.14159265359*(radio*radio)
+print("Su area es:")
+print(area)
---
+++ ejercicio4.py
@@ -0,0 +1,3 @@
+numero=int(input("Ingrese e numero a calcular la raiz: "))
+raiz=numero**0.5
+print(raiz)
